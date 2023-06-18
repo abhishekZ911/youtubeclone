d5e6fd14d63e39ce8b345ada5d4380b707d39fa2
Abhishek Kumar Keshri <abhikeshri0888@gmail.com>
2023-06-18 23:01:03 +0530

    Completed the functionalities part. UI left only.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Navbar from './Navbar';
 import './App.css';
 import Home from './home';
+import { Routes, Route, Navigate } from "react-router-dom"; 
+import VideoComponent from './VideoComponent';
 
 function App() {
+
+
+
   return (<>
     <Navbar/>
-    <Home/>
+    <Routes> 
+      <Route path='/' element={<Navigate to='/1' />}></Route>  
+      <Route path=':pageId' element={<Home/>}></Route>      
+      <Route exact path=':pageId/:videoId' element={<VideoComponent/>}></Route>
+    </Routes>
     </>
   );
 }
